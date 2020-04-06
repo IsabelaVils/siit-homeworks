be0c8e9c6aa749b255b79d43b4a77ea63748d413
Isabela Vilsanescu <isabela.vilsanescu@gmail.com>
2020-04-06 15:55:48 +0300

    Am adaugat fisierul modificat

--- src/components/movies/MovieDetails.js
+++ src/components/movies/MovieDetails.js
@@ -18,9 +18,34 @@ function MovieDetails() {
     if(movie) {
         console.log(movie);
         return (
-            <>
-                <h1>{ movie.Title }</h1>
-                <h5>Year: { movie.Year }</h5>
+            <>  
+                <div className="container " className = "bg-secondary text-white col-md-12" >
+                    <div className="row" >
+                        <div className= "col-md-6" >
+                            <h1>{ movie.Title}</h1><span className="font-weight-light font-size-1.2rem"> ({ movie.Year })</span>
+                            <p> {movie.Genre}</p>
+                        </div>
+                        <div className="col-md-6">
+                            <p className ="font-weight-bold"> {movie.imdbRating}<span className="font-weight-light">/10</span></p>
+                        </div>                  
+                    </div>
+
+                    <div className="row">
+                            <div className="col-md-4 ">
+                                <img className="card-img-top-00" src={ movie.Poster } alt="Movie Poster"/>  
+                            </div>
+                            <div className="col-md-8 ">
+                                    <p><span className="font-weight-bold"> Plot</span>: {movie.Plot}</p>
+                                    <p> <span className="font-weight-bold">Director:</span> { movie.Director }</p>
+                                    <p><span className="font-weight-bold">Writer:</span> {movie.Writer}</p>
+                                    <p><span className="font-weight-bold">Actors:</span> {movie.Actors}</p>
+                                    <p><span className="font-weight-bold">Ratings:</span> {movie.Ratings[0].Value}, {movie.Ratings[1].Value}, 
+                                    {movie.Ratings[2].Value}
+                                    </p>
+                            </div>
+                    </div>
+                </div>               
+               
             </>
         );
     } else {
